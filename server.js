bf5f3ee475096d749f8779edc116cc5a3c408062
Ryan <rfoss28@comcast.net>
2018-09-08 13:49:18 -0400

    corrected port

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -93,3 +94,4 @@ app.post("/api/characters", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+})
